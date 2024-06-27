6ea15931b76d365541f4b80daf0aa007bfa038b8
acbray <abray039@gmail.com>
2024-06-27 22:34:28 +0200

    Inital page setup

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".aboutMe", {
+  strings: ["Alec Bray", "a Backend Developer", "dabbling in Frontend"],
+  smartBackspace: true,
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+  loopCount: Infinity,
+  startDelay: 1000,
+});
