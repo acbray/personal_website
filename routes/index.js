a43a06e14d9d88cad7255629fa7c2f9d5ebfafff
acbray <abray039@gmail.com>
2024-07-03 17:06:06 +0200

    Updates 'about' section | implements contact form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
+var express = require("express");
+const { error } = require("jquery");
 var router = express.Router();
+var nodemailer = require("nodemailer");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index");
+});
+
+// Route handler for submitting contact form
+router.post("/contact", (req, res) => {
+  const { name, email, message } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: process.env.ADMIN_EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: email,
+    to: process.env.ADMIN_EMAIL,
+    subject: `New message from ${name}`,
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      return res.status(500).send("Error sending message.");
+    }
+    res.status(200).send("Message sent.");
+  });
 });
 
 module.exports = router;
